6f5a1b4fd5d7528597edb4d9485b412d89a69d11
ElkkuTheBaws <58703912+ElkkuTheBaws@users.noreply.github.com>
2023-02-25 12:23:39 +0200

    Changes

--- blogilista_server/app.js
+++ blogilista_server/app.js
@@ -1,44 +1,26 @@
+const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
 const mongoose = require('mongoose')
 
 
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
+mongoose.set('strictQuery', false)
+const mongoUrl = config.MONGODB_URI
+mongoose.connect(mongoUrl)
+  .then(() => {
+    console.log("connected to mongo db successfully")
+  })
+  .catch((error) => {
+    console.log("connection error: ", error.message)
+  })
+
 
 
 app.use(cors())
 app.use(express.json())
-
-
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = 'mongodb://localhost/bloglist'
-mongoose.connect(mongoUrl)
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('api/blogs', blogsRouter)
 
 module.exports = app
--- blogilista_server/index.js
+++ blogilista_server/index.js
@@ -1,8 +1,9 @@
 const http = require('http')
+const config = require('./utils/config')
 const app = require('./app')
-//https://fullstackopen.com/osa4/sovelluksen_rakenne_ja_testauksen_alkeet#tehtavat-4-1-4-2
 
-const PORT = 3003
+
+const PORT = config.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
